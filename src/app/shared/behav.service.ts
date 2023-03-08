8dbb9c1c6c6c084f945a94f0c11d1e4512077e05
toth julia <t.julia982@gmail.com>
2023-03-08 09:10:31 +0100

    átadás működik

--- src/app/shared/behav.service.ts
+++ src/app/shared/behav.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BehavService {
-
+  private data = new BehaviorSubject('');
+  currentData = this.data.asObservable(); //observable: ennek az adatnak az értékére subscribbal így lehet feliratkozni
   constructor() { }
+
+  setData(data:any){
+    this.data.next(data);
+  }
 }
